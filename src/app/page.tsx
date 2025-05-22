949d0ddab75fe8c0e9b5f9b9c343d4079f9a4570
rayanemelo <raymeloc@gmail.com>
2025-05-22 17:26:53 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,115 @@
+import Image from "next/image";
+
+export default function Home() {
+
+  const brindes = [
+    {
+      id: 1,
+      name: "Self Ring Light",
+      image: "/image001.png",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=10&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 2,
+      name: "Porta Cartão RFID",
+      image: "/image002.png",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=11&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 3,
+      name: "Caneca Inox",
+      image: "/image003.png",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=14&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 4,
+      name: "Estojo",
+      image: "/image004.png",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=15&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 5,
+      name: "Kit Viagem",
+      image: "/image005.png",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=16&qty=1&seller=1&sc=1",
+    }
+  ]
+
+  const produtos = [
+    {
+      id: 1,
+      name: "Borracha",
+      image: "/borracha.webp",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=5&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 2,
+      name: "Caneta",
+      image: "/caneta-azul.jpg",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=2&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 3,
+      name: "Caneta Hidrográfica",
+      image: "/caneta.png",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=7&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 4,
+      name: "Estojo",
+      image: "/estojo.png",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=6&qty=1&seller=1&sc=1",
+    },
+    {
+      id: 5,
+      name: "Lapiseira",
+      image: "/lapiseira.jpg",
+      link: "https://cybersourcepartnerbr.myvtex.com/checkout/cart/add/?sku=3&qty=1&seller=1&sc=1",
+    }
+  ]
+
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 p-4">
+      {brindes.map((product) => (
+        <a
+          key={product.id}
+          className="flex flex-col items-center gap-2 hover:underline hover:underline-offset-4"
+          href={product.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src={product.image}
+            alt={product.name}
+            width={200}
+            height={200}
+            className="rounded-lg"
+          />
+          {product.name}
+        </a>
+      ))}
+      <div className="col-span-5 h-px bg-gray-300 my-4" />
+      {produtos.map((product) => (
+        <a
+          key={product.id}
+          className="flex flex-col items-center gap-2 hover:underline hover:underline-offset-4"
+          href={product.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src={product.image}
+            alt={product.name}
+            width={200}
+            height={200}
+            className="rounded-lg"
+          />
+          {product.name}
+        </a>
+      ))}
+    </div>
+  );
+}
